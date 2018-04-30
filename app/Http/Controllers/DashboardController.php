13611b55818e2d7bd3f25c23dea85d528616884d
b-henderson <benhenderson976@gmail.com>
2018-04-30 20:29:48 +0100

    initial commit

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Routing\Controller as BaseController;
+use View;
+
+class DashboardController extends BaseController
+{
+    public function index()
+    {
+        return View::make('dashboard');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+ */
+
+Route::get('/', array(
+    'as'   => 'dashboard',
+    'uses' => 'DashboardController@index',
+));
+
+Route::get('/shortner', array(
+    'as'   => 'short',
+    'uses' => 'ShortController@index',
+));
+Route::post('/shortner/make', array(
+    'as'   => 'make',
+    'uses' => 'LinkController@make',
+));
+Route::get('/shortner/{code}', array(
+    'as'   => 'get',
+    'uses' => 'LinkController@get',
+));
